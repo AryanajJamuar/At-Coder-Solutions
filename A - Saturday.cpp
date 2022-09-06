fefae6ac2998b7380dbc646a668a699715f26029
AryanajJamuar <86473851+AryanajJamuar@users.noreply.github.com>
2022-09-06 10:00:14 +0530

    Create A - Saturday.cpp

---
+++ A - Saturday.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath> 
+#include <algorithm>
+#include <vector>
+#include <bits/stdc++.h>
+using namespace std ;
+typedef long long int ll ;
+int main()
+{ string s ;
+  cin>>s ;
+  if(s=="Monday")
+    cout<<5 ;
+    if(s=="Tuesday")
+       cout<<4 ;
+       if(s=="Wednesday")
+           cout<<3 ;
+           if(s=="Thursday")
+             cout<<2 ;
+             if(s=="Friday")
+                cout<<1 ;
+             return 0;
+}
